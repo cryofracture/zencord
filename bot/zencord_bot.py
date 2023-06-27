0a40b96f50522df0f8071af80da6b16936bf97c5
Taylor Acree <thecryoforce@gmail.com>
2023-06-26 20:41:57 -0500

    Add email CC to ticket request, change wording a bit. Update ticket from discord also now works.

--- bot/zencord_bot.py
+++ bot/zencord_bot.py
@@ -65,11 +65,11 @@ async def support_ticket_command(ctx, *args):
         subject_response = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
         subject = subject_response.content
 
-        # await ctx.author.send(
-        #     f"Please provide your email address: "
-        # )
-        # email_response = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
-        # email = email_response.content
+        await ctx.author.send(
+            f"Please provide your email address to receive ticket updates: "
+        )
+        email_response = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        email = email_response.content
 
         try:
             new_ticket = open_new_ticket(description, subject, ctx.author.name, email)
@@ -79,7 +79,7 @@ async def support_ticket_command(ctx, *args):
             f"All right, {ctx.author.name}, I have created a new ticket for you and the Casper Labs Support team will begin reviewing your issue and get back to you soon."
         )
         await ctx.author.send(
-            f"The new ticket ID is {new_ticket}. You can check for updates to the ticket from the support team using the command: $get_ticket_updates"
+            f"The new ticket ID is {new_ticket}. You can check for updates to the ticket from the support team directly to your email address."
         )
         user_id = ctx.author.id
         active_tickets[new_ticket] = user_id
@@ -87,7 +87,7 @@ async def support_ticket_command(ctx, *args):
         await ctx.author.send("Okay, please re-enter your issue description if you'd like to open a support ticket.")
 
 
-def open_new_ticket(request_description: str, request_subject: str, requester_name: str):
+def open_new_ticket(request_description: str, request_subject: str, requester_name: str, requester_email):
     zd_email = os.environ['ZD_EMAIL']
     zd_api_token = os.environ['ZD_TOKEN']
     zd_subdomain = os.environ['ZD_DOMAIN']
@@ -101,8 +101,9 @@ def open_new_ticket(request_description: str, request_subject: str, requester_na
             },
             "subject": request_subject,
             "requester": {
-                "name": requester_name
-            }
+                "name": requester_name,
+                "email": requester_email
+            },
         }
     }
 
